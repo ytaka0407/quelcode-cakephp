6a69ef4b035119ae9d9b139a744d286c9f17d52a
ytaka0407 <spicayt@gmail.com>
2021-01-15 23:37:31 +0900

    initializeアクションで各初期値を設定

--- html/mycakeapp/src/Model/Table/PeopleTable.php
+++ html/mycakeapp/src/Model/Table/PeopleTable.php
@@ -6,4 +6,13 @@ use Cake\ORM\Table;
 
 class PeopleTable extends Table
 {
+
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('people');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+    }
 }
