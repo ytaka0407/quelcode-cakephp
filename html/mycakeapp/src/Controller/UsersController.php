dba6611ed7f40e105298277b704a7a82d2b36fca
ytaka0407 <spicayt@gmail.com>
2021-01-21 12:37:57 +0900

    認証・認可アクションを実装

--- html/mycakeapp/src/Controller/UsersController.php
+++ html/mycakeapp/src/Controller/UsersController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Event\Event;
 
 /**
  * Users Controller
@@ -12,6 +15,84 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('Flash');
+        $this->loadComponent(
+            'Auth',
+            [
+                'authrize' => ['Controller'],
+                'authenticate' => [
+                    'Form' => [
+                        'fields' => [
+                            'username' => 'username',
+                            'password' => 'password'
+                        ]
+                    ]
+                ]
+            ],
+            [
+                'loginRedirect' => [
+                    'controller' => 'Users',
+                    'action' => 'login'
+                ],
+                'logoutRedirect' => [
+                    'controller' => 'Users',
+                    'action' => 'logout'
+                ],
+                'authError' => 'ログインしてください。',
+            ]
+        );
+    }
+
+    //ログイン処理
+    function login()
+    {
+        if ($this->request->isPost()) {
+            $user = $this->Auth->identify();
+
+            if (!empty($user)) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('ユーザー名かパスワードが間違っています。');
+        }
+    }
+
+
+    //ログアウト処理
+    public function logout()
+    {
+        $this->request->session()->destroy();
+        return $this->redirect($this->Auth->logout());
+    }
+
+    //認証を使わないページ設定
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow(['login', 'index', 'add']);
+    }
+
+    //認証時のロールチェック
+    public function isAuthorized($user = null)
+    {
+        if ($user === 'admin') {
+            return true;
+        }
+
+        if ($user === 'user') {
+            return false;
+        }
+
+        if ($user === null) {
+            return false;
+        }
+    }
+
     /**
      * Index method
      *
